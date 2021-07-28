5abb19fed7b3b182c00d9b7c1b13e91987b7bb55
pthabot <prima.fams@gmail.com>
2021-07-28 15:36:15 +0700

    i-love-coding prima

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // Kerjakan kode kalian di bawah ini
+console.log ("========= Looping Pertama For ========");
+
+for (no = 1 ; no <= 20; no++) {
+    console.log (no + " " + '-'+ " " +'Coding Asik Banget Cuy !');
+}
+
+console.log ("======================================");
+
+console.log ("========= Looping Kedua For ===============");
+
+for (no = 20 ; no >= 1; no--) {
+    console.log (no + " " + '-'+ " " +'Coding itu harus latihan setiap hari!');
+}
+
+console.log ("===========================================");
+
+console.log ("========= Looping Pertama While ===========");
+
+var no = 2;
+while (no <= 20) {
+    console.log(no + " " + "-" + " " + 'Coding Asik Banget Coy !');
+    no = no + 2; 
+}
+console.log ("========================================");
+
+console.log ("========= Looping Kedua While =============");
+
+var no = 20;
+while (no >= 2) {
+    console.log(no + " " + "-" + " " + 'Coding itu harus latihan setiap hari!');
+    no = no - 2;
+}
+console.log ("===========================================");
