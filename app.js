33357469b6081cfd2f7197c33955a2df8d84ff92
rezaagust <reza.2020@smk-strada.sch.id>
2021-08-04 15:49:38 +0700

    tugas reza

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // Kerjakan kode kalian di bawah ini
+console.log ("========= Looping Pertama For ========");
+
+for (no = 1 ; no <= 20; no++) {
+    console.log (no + " " + '-'+ " " +'Coding Asik Banget Cuy !');
+}
+
+console.log ("======================================");
+
+console.log ("========= Looping Kedua For ===============");
+
+for (no = 20 ; no >= 1; no--) {
+    console.log (no + " " + '-'+ " " +'Coding itu harus latihan setiap hari!');
+}
+
+console.log ("===========================================");
+
