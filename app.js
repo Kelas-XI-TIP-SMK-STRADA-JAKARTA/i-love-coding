3d59ef07426d3e5560d4ecb98b55c430c9600e16
myairin <irenewindagultom@gmail.com>
2021-08-04 09:54:21 +0700

    Tugas pertama Perulangan telah selesai

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // Kerjakan kode kalian di bawah ini
+
+
+
+for (no = 1; no <= 20; no++) {
+    console.log(no + " - " + "Coding Asik Banget Cuy !");
+}
+console.log("======================");
+console.log(" pengulangan for ke 2 ");
+
+for (no = 20; no >= 1; no--) {
+    console.log(no + " - " + "Coding itu harus latihan setiap hari!");
+}
+
+
