c4a314ef6cdc9120c4a30207f2a5beb3005bce14
zkarine <zefanyakarine03@gmail.com>
2021-08-16 14:42:52 -0700

    Zefanya karine

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // Kerjakan kode kalian di bawah ini
+console.log("                                       ")
+console.log("perulangan 1")
+console.log("                                       ")
+    for (no = 1; no <= 20; no++) {
+        console.log(no + "  " + 'coding asik banget cuy!');
+    }
+
+console.log("                                       ")
+console.log("perulangan 2")
+console.log("                                       ")
+    for (no = 20; no >= 1; no--) {
+        console.log(no + "  " + 'coding itu harus latihan setiap hari!');
+    }
+
+   
